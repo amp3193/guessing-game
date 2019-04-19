8e7808864cecea10b15e2a1de93bd09434e259e1
amp3193 <amp3193@yahoo.com>
2019-04-18 21:16:30 -0400

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,37 +5,74 @@ let guessList = [];
 let winCount = 0;
 let lossCount = 0;
 
-function start(){
-    $("#wins").text(winCount);
-    $("#loses").text(lossCount);
-    $("guessesLeft").text(guessCount);
-    $("letters").text(letter);
+const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function display(){
+    $("#wins").text("Wins: " + winCount);
+    $("#losses").text("Losses: " + lossCount);
+    $("#guessesLeft").text("Guesses Left: " + (maxGuesses - guessCount));
+    $("#letters").text("Guess List:" + guessList);
 }
 
-onkeypress = function(event) {
+function start() {
+    //generates a random letter answer
+    let index = Math.floor(Math.random() * alpha.length);
+    answer = alpha[index];
+    guessCount = 0;
+    guessList = [];
+    display();
+}
 
-    //person guess
-    let value = event.charCode || event.keyCode;
-    let letter = String.fromCharCode(value);
-    guessList.push(letter);
-    alert("Your guesses" + guessList);
-    
+function guessTests(letter) {
+    let result = false;
 
+    if (isValidLetter(letter)) {
+        
+        if (!hasBeenGuessed(letter)) {
+            result = true;
+        } else {
+            alert("You already guessed that letter!");
+        }
+    } else {
+        alert("That's not a letter!");
+    }
+       
+    return result;
+}
 
+function hasBeenGuessed(letter) {
+    let index = guessList.indexOf(letter);
+    let found = (index > -1);
 
+    return found;
+}
 
-};
+function isValidLetter(letter) {
+    let index = alpha.indexOf(letter);
+    let found = (index > -1);
 
+    return found;
+}
 
+function handleUserInput(event) {
+    let letter = event.key.toLowerCase();
+    if (guessTests(letter)) {
+        guessCount = guessList.push(letter);
+        gameStatus();
+    }
+    display();
+}
 
+function gameStatus() {
+    if (hasBeenGuessed(answer)) {
+        winCount++;
+        start();
+    } else if (guessCount >= maxGuesses) {
+        lossCount++;
+        start();
+    } 
+}
 
+onkeypress = handleUserInput;
 
-
-/* <p id="wins"></p>
-<p id="loses"></p>
-<p id="guessesLeft"></p>
-<p id="guessesUsed"></p> */
-
-
-
-
+start();
